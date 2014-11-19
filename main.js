9ea85d205d6ac70e9a0dd18d1bc00cd49d6a87c5
Daniel Grondin <tm.kkitsune@gmail.com>
2014-11-19 10:57:30 -0500

    Added koa-logger to the server

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ var koa = require("koa"),
 	serve = require("koa-static"),
 	mount = require("koa-mount"),
 	route = require("koa-router"),
+	logger = require("koa-logger"),
 	NeDB = require("nedb");
 
-var app = koa();
+var app = koa().use(logger());
 app.use(serve(__dirname + "/html/"));
 
 app.listen(1337);
