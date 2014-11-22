0b53460ba913d8a6cfb49c48e7cc9a0b68fe9675
unknown <grod270789@ETU2012-14.tech-info.local>
2014-11-22 05:10:06 -0500

    Added some basic api for the databases
    Created some dummy databases

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
 var koa = require("koa"),
 	serve = require("koa-static"),
 	mount = require("koa-mount"),
-	route = require("koa-router"),
+	router = require("koa-router"),
 	logger = require("koa-logger"),
 	NeDB = require("nedb");
 
+var db = new Object();
+db.skills = new NeDB({
+	filename: __dirname + "/database/skills.db",
+	autoload: true
+});
+db.spells = new NeDB({
+	filename: __dirname + "/database/spells.db",
+	autoload: true
+});
+db.classes = new NeDB({
+	filename: __dirname + "/database/classes.db",
+	autoload: true
+});
+db.feats = new NeDB({
+	filename: __dirname + "/database/feats.db",
+	autoload: true
+});
+db.races = new NeDB({
+	filename: __dirname + "/database/races.db",
+	autoload: true
+});
+
+var api = koa();
+api.use(router(api));
+
+api.get("/", function * (next) {
+	this.body = {
+		version: "0.1.0",
+		databases: [
+			"skills",
+			"spells",
+			"classes",
+			"feats",
+			"races"
+		]
+	}
+});
+
+api.get("/skill", function * (next) {
+	var data = {};
+	db.skills.find({}, function (err, docs) {
+		data.docs = docs;
+		data.err = err;
+	});
+	this.body = data;
+});
+
 var app = koa().use(logger());
+app.use(mount("/api", api));
 app.use(serve(__dirname + "/html/"));
 
 app.listen(1337);
