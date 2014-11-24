32c903fad0cdac27223f9803f4e8300ba0e83227
Daniel Grondin <tm.kkitsune@gmail.com>
2014-11-24 10:55:34 -0500

    API server now closes when application ends

--- authoring-tools/app.js
+++ authoring-tools/app.js
@@ -1,7 +1,13 @@
 var gui = require("nw.gui"),
-	NeDB = require("nedb"),
+	http = require("http"),
 	api = require("./authoring-api.js");
 
-api.listen(1337);
+var server = http.createServer(api.callback());
+server.listen(1337);
+
+gui.Window.get().on("close", function () {
+	server.stop();
+	this.close(true);
+});
 
 var app = angular.module("AuthoringTools", ["ui.bootstrap"]);
